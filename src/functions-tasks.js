81624864619b9ba2a53f14d20b9c060c1fa63274
Yana Malakhova <ya6ka0@gmail.com>
2025-04-14 22:50:47 +0300

    feat: write memoize solution

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -109,8 +109,18 @@ function getPolynom() {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  let result;
+  let isCached = false;
+
+  return () => {
+    if (!isCached) {
+      result = func();
+      isCached = true;
+    }
+
+    return result;
+  };
 }
 
 /**
